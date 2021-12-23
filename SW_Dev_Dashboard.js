3208c59870d45dbafd8b526c09fe699675b1b860
Abdullah A Shaker <33979966+soashaker@users.noreply.github.com>
2021-12-23 09:45:55 -0500

    Delete SW_Dev_Dashboard.js

--- SW_Dev_Dashboard.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-function onRequest (request, response) {
-     response.writeHead (200, {'Content-Type': 'text/plain'});
-     response.write('SW Dev Dashboard For Delivery Drone Software System');
-     response.end();
-}
-http.createServer(onRequest).listen(8000);
